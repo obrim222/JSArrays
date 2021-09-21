62ff1e1d84fb8b6b19cd624393913139a3daadce
Michael O Brien <mobrienoct1981@gmail.com>
2021-09-21 22:17:22 +0200

    small change

--- index.js
+++ index.js
@@ -22,42 +22,66 @@ var AgeInput = document.getElementById("age-box");
 var messageBox = document.getElementById("display");
 
 const getUserlist = function () {
-  for(let i =0; i < userList.length; i++) {
-    document.getElementById("tableBody").insertAdjacentHTML("beforeend","<tr><td>"+userList[i].firstname+"</td><td>"+userList[i].lastname+"</td><td>"+userList[i].age+"</td></tr>");
+  for (let i = 0; i < userList.length; i++) {
+    document
+      .getElementById("tableBody")
+      .insertAdjacentHTML(
+        "beforeend",
+        "<tr><td>" +
+          userList[i].firstname +
+          "</td><td>" +
+          userList[i].lastname +
+          "</td><td>" +
+          userList[i].age +
+          "</td></tr>"
+      );
   }
 };
 
 const getLastUser = function () {
-  i = userList.length-1;
-  document.getElementById("tableBody").insertAdjacentHTML("beforeend","<tr><td>"+userList[i].firstname+"</td><td>"+userList[i].lastname+"</td><td>"+userList[i].age+"</td></tr>");
+  i = userList.length - 1;
+  document
+    .getElementById("tableBody")
+    .insertAdjacentHTML(
+      "beforeend",
+      "<tr><td>" +
+        userList[i].firstname +
+        "</td><td>" +
+        userList[i].lastname +
+        "</td><td>" +
+        userList[i].age +
+        "</td></tr>"
+    );
 };
 
 const welcomeMessage = function () {
-  let i = userList.length-1;
-  document.getElementById("message").outerHTML ="<div id='message'><p> Welcome "+userList[i].firstname+" "+userList[i].lastname+" ,we are happy that you are here !! </p></div>";
+  let i = userList.length - 1;
+  document.getElementById("message").outerHTML =
+    "<div id='message'><p> Welcome " +
+    userList[i].firstname +
+    " " +
+    userList[i].lastname +
+    " ,we are happy that you are here !! </p></div>";
 };
 
 function insert() {
-  
-  var firstName = document.getElementById('fn-box').value;
-  var lastName = document.getElementById('ln-box').value;
-  var age = document.getElementById('age-box').value;
+  var firstName = document.getElementById("fn-box").value;
+  var lastName = document.getElementById("ln-box").value;
+  var age = document.getElementById("age-box").value;
   console.log("FirstName:", firstName, "LastName:", lastName, "Age:", age);
   //create User Object
-  let person = {firstname: firstName, lastname: lastName, age: age};
+  let person = { firstname: firstName, lastname: lastName, age: age };
   userList.push(person);
   console.log(userList);
   //insert2();
-  alert("Welcome: " + firstName + " there are " + userList.length + " users connected !");
+  alert(
+    "Welcome: " +
+      firstName +
+      " there are " +
+      userList.length +
+      " users connected !"
+  );
   //getUserlist();
   getLastUser();
   welcomeMessage();
-}
-
-function insert2() {
-  userList.push({
-    firstname: firstname[0],
-    lastname: lastnames[0],
-    age: ages[0],
-  });
 }
